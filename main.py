24aa6905eaedc86d8fb872e499844051b71f3459
Alejandro Miron <113551832+amironromer@users.noreply.github.com>
2023-02-15 20:20:30 +0000

    slides

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 # remember to fork this and push to github
 
+# f = open('test.txt', "r")
+# print(f.readlines())
+# f.close()
+
+# with open('test.txt', 'r') as f:
+#   f_contents = f.readlines()
+#   print(f_contents, end='')
+
+# employee_file = open("employees.txt", "r")
+# print(employee_file.readlines())
+# employee_file.close()
+
+# employee_file = open("employees.txt", "r")
+# print(employee_file.readlines()[1])
+# employee_file.close()
+
+employee_file = open("employees.txt", "r")
+for employee in employee_file.readlines():
+  print(employee)
+employee_file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
